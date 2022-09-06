91c1cdbbee9c443453f4390c72a3771d761874b9
unknown <greatgrant116@gmail.com>
2022-09-06 16:35:56 +0100

    generate_random_numbers test case pass

--- Getting-started-with-javascript-programs/Week-1/getting-started-with-javascript-master/src/generate_random_numbers.js
+++ Getting-started-with-javascript-programs/Week-1/getting-started-with-javascript-master/src/generate_random_numbers.js
@@ -12,8 +12,14 @@ for any non-numeric value passed to the function.
 
 */
 
-module.exports = function generateRandomNumber() {
-   
+module.exports = function generateRandomNumber(min, max) {
   // Provide Solution Code Here
-
-}
+  const num = 0;
+  if (typeof min === typeof num || typeof max === typeof num) {
+    if (min >= max) {
+      return 'Incorrect Inputs. 1st value should be less than the second';
+    } else {
+      return Math.floor(Math.random() * (max - min + 1) + min);
+    }
+  } else return 'Invalid Input Types, All Inputs Should Be of Type Number !!';
+};
