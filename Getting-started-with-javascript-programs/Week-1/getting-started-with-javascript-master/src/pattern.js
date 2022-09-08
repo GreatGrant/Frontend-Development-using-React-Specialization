b6d6977489ead791b5ba364a1bfcfe1145e0e538
unknown <greatgrant116@gmail.com>
2022-09-08 13:43:23 +0100

    string_replace test case pass

--- Getting-started-with-javascript-programs/Week-1/getting-started-with-javascript-master/src/pattern.js
+++ Getting-started-with-javascript-programs/Week-1/getting-started-with-javascript-master/src/pattern.js
@@ -20,7 +20,18 @@ if non-numeric value is passed to the function.
 module.exports = function drawPattern(noOfRows) {
 
   // Provide Solution Code Here
-  for(i in   
-    
-  )
+
+// External loop
+for (let i = 1; i <= n; i++) {
+  // printing spaces
+  for (let j = 1; j <= n - i; j++) {
+    process.stdout.write(' ')
+  }
+  // printing stars
+  for (let k = 0; k < 2 * i - 1; k++) {
+    process.stdout.write('*')
+  }
+  console.log();
+}
+  
 }
